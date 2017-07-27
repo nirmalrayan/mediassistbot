96dad3bd46ddeacba031a2c14ab4d5432b4786ab
nirmalrayan <nirmal_rayan@yahoo.com>
2017-07-27 12:34:41 +0530

    Created package.json and app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({ appId: '021235f4-edd2-4ad6-8ac6-d38076e4cfa0', appPassword: 'aSt0XQJAbRAkfVEoWdMsfEG' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
