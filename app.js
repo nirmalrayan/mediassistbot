963f234af733b93100b7f920a7f650cc63926338
nirmalrayan <nirmal_rayan@yahoo.com>
2017-08-16 12:45:33 +0530

    Trying to add index.html again

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('env2')('.env'); // loads all entries into process.env
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || process.env.PORT || 3000, function() 
+server.listen(process.env.PORT || process.env.port || 3000 || 3978, function() 
 {
    console.log('%s listening to %s', server.name, server.url); 
 });
