5ec47988d5d69c22237379e39f9f24664972b6f7
nirmalrayan <nirmal_rayan@yahoo.com>
2017-08-21 11:40:25 +0530

    Added HTML Geolocation 10B

--- app.js
+++ app.js
@@ -1131,6 +1131,6 @@ server.use(restify.bodyParser({ mapParams: false })); */
 
 server.post('/location', function(req, res){
 //	console.log("Got some lat: " + req.body.lat + " and some long:" + req.body.lng);
-	console.log("Entire request: "+ JSON.stringify(req));
+	console.log("Entire request: "+ req);
 	return next();
 });
