9c3cbd6bd00dd283a4dec2473ce02fffe194e924
nirmalrayan <nirmal_rayan@yahoo.com>
2017-08-21 19:09:32 +0530

    Testing socket connection 3

--- app.js
+++ app.js
@@ -1104,10 +1104,10 @@ server.post('/location', function(req, res){
 		response.end('Location data stored successfully\n');
 		console.log("Stored lat and long: " + response);
 	}); */
-	res.setCookie('lat', JSON.stringify(req.body.lat)),
-	res.setCookie('lng', JSON.stringify(req.body.lng));
+	res.setCookie('lat', req.body.lat),
+	res.setCookie('lng', req.body.lng);
 	
-	res.send(JSON.stringify(cookies));
+	console.log(JSON.stringify(cookies));
 
 });
 
