6e42d05dba60153bc522c80d2434ee1ad07610bf
nirmalrayan <nirmal_rayan@yahoo.com>
2017-08-21 12:39:40 +0530

    Added HTML Geolocation 10e

--- app.js
+++ app.js
@@ -1063,6 +1063,8 @@ server.use(restifyBodyParser({ mapParams: true }));
 server.post('/location', function(req, res){
 //	console.log("Got some lat: " + req.body.lat + " and some long:" + req.body.lng);
 	console.log("Entire request: Lat-"+ JSON.stringify(req.body.lat) + " & Long-" + JSON.stringify(req.body.lng));
-	session.userData.latitude = JSON.stringify(req.body.lat);
-	session.userData.longitude = JSON.stringify(req.body.lng);
+	function(session){
+		session.userData.latitude = JSON.stringify(req.body.lat);
+		session.userData.longitude = JSON.stringify(req.body.lng);
+	}
 });
