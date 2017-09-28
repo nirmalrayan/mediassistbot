afe8853ca709933a8a4fd9d9633bc8eb0c040049
nirmalrayan <nirmal_rayan@yahoo.com>
2017-09-28 17:14:26 +0530

    git cache cleared

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ server.listen(process.env.PORT || 3000, function()
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.queryParser());
 
-
 // Create chat bot
 var connector = new builder.ChatConnector
 ({ appId: process.env.MY_APP_ID, appPassword: process.env.MY_APP_PASSWORD }); 
