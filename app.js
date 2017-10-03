ac56dac08ff5b570efd719775faed0960221f3f9
nirmalrayan <nirmal_rayan@yahoo.com>
2017-10-03 14:47:05 +0530

    fixing bug 5

--- app.js
+++ app.js
@@ -91,13 +91,13 @@ var bot = new builder.UniversalBot(connector,
  directory: __dirname,
  default: '/index.html'	
 })); 
-
-
-console.log('Connector data: '+ JSON.stringify(bot));
 	
 //LUIS Configuration
 var recognizer = new builder.LuisRecognizer("https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/4e0df9eb-a11f-495d-8e90-b0579fde9b86?subscription-key=5ccd61decaf04a0caff771ac48a46ded&timezoneOffset=330&verbose=true&q=");
 //bot.recognizer(recog);
+
+
+console.log('Connector data: '+ JSON.stringify(recognizer));
 
 bot.dialog('/refer', new builder.IntentDialog({ recognizers : [recognizer]})
     .matches("SayHello", "hello")
