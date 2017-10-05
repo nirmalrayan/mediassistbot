c60f4fa09b6c8a2f85f2da50f1dd7e9d14df69bd
nirmalrayan <nirmal_rayan@yahoo.com>
2017-10-05 22:45:00 +0530

    Fully functional Medibot with LUIS integration and FB 4

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ server.use(restify.plugins.queryParser());
 var connector = new builder.ChatConnector
 ({ appId: process.env.MY_APP_ID, appPassword: process.env.MY_APP_PASSWORD }); 
 
-
 //MAIN.
 var bot = new builder.UniversalBot(connector,
 
