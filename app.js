d81d07dfe0a14759bc9bc8e8171ec7f157014651
nirmalrayan <nirmal_rayan@yahoo.com>
2017-10-09 13:20:39 +0530

    Trying FB Connection fix 2

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var bot = new builder.UniversalBot(connector,
     function (session) {
 		
 		if(session.message.address.channelId === 'facebook'){
-		/*	var welcomeCard = new builder.HeroCard(session)
+			var welcomeCard = new builder.HeroCard(session)
 				.title("Hi %s! Nice to see you. I am MediBot", session.message.address.user.name)
 				.subtitle("I will be your personal healthcare assistant. ℹ️ Type \"show menu\" or \"#\" at any time to see the menu.")
 				.images([
@@ -50,8 +50,7 @@ var bot = new builder.UniversalBot(connector,
 				.buttons([
 					builder.CardAction.imBack(session, "Show Menu", "Show Menu")
 				]);
-			session.userData.name = session.message.address.user.name;*/
-			session.send("You are using facebook messenger to chat");
+			session.userData.masterName = session.message.address.user.name;
 		}
 		else{
 			if(session.userData.masterName){
