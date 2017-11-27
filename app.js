5223e76ea00ea894e96cf3b7c26988f490d68f2d
nirmalrayan <nirmal_rayan@yahoo.com>
2017-11-27 16:11:16 +0530

    Changing MS Credentials 13

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const config = {
 // Setup Restify Server
 
 var server = restify.createServer();
-server.listen(process.env.PORT || process.env.port || 4321, function() 
+server.listen(process.env.PORT || process.env.port || 3000, function() 
 {
    console.log('%s listening to %s', server.name, server.url); 
 });
