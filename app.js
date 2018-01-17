11ccf38cb5212891a38082db100d4d4c40aa6aee
nirmalrayan <nirmal_rayan@yahoo.com>
2018-01-17 21:14:22 +0530

    Fixing Feedback 4

--- app.js
+++ app.js
@@ -613,7 +613,7 @@ bot.dialog('askforFeedback',[
 				{
 					console.log('This is session.message data' + JSON.stringify(session.message));
 					var serviceName = JSON.stringify("Track Claim with ID");
-					storeFeedback(JSON.stringify(session.message.user.id).replace(/"/g, "'"), JSON.stringify(session.message.user.name).replace(/"/g, "'"), serviceName.replace(/"/g, "'"), wasHelpful,JSON.stringify('OK').replace(/"/g, "'"),JSON.stringify('5').replace(/"/g, "'"));
+					storeFeedback(JSON.stringify(session.message.user.id).replace(/"/g, "'"), JSON.stringify('Web User').replace(/"/g, "'"), serviceName.replace(/"/g, "'"), wasHelpful,JSON.stringify('OK').replace(/"/g, "'"),JSON.stringify('5').replace(/"/g, "'"));
 				}
 			}
 			);
